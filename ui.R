20b562a7b922dd4e9f58a16977f0c9f1f5109a07
Jeff Bruce <jeffbruce@gmail.com>
2015-05-25 16:51:42 -0400

    Added collaborator and institution list to the first page.

--- ui.R
+++ ui.R
@@ -11,7 +11,7 @@ shinyUI(
       
       titlePanel('Clustering Autism in the Mouse'),
       
-      br(),
+      hr(),
              
       p('This web app presents results from the research paper by Ellegood et al. (2014) published in Molecular Psychiatry, entitled: ', em('Clustering autism: 
         using neuroanatomical differences in 26 mouse models to gain insight into the heterogeneity.'), '  In the ', em('Overview '),
@@ -21,23 +21,66 @@ shinyUI(
       p('By making the data open and public, our aim is to allow researchers and interested parties to help generate and test their own 
         hypotheses with our data to help tease apart the complex heterogeneity and etiology at play in autism spectrum disorder.'),
       
-      p('Research efforts are ongoing using additional mouse models of autism, adding to the long list of strains which were scanned 
-        and analyzed in the original work.  This research could not have been done without the help from a long list of people, 
-        who we would like to recognize for their great work:'),
+      p('Below, you will find the aforementioned paper\'s figures along with a brief description summarizing the message of each figure.
+        There is also a brief methods section describing very generally the properties of the mice, how scans were performed, and additional
+        details.'),
       
-      ul('Jacob Ellegood', 'Jason Lerch'),
-      
-      p('ADD IN AWESOME COLLABORATORS HERE'),
-      
-      p('Below, you will find the key figures in the aforementioned paper'),
-      
-      br(),
+      hr(),
       
       img(src='/images/figure1.png', align = "center"),
       img(src='/images/figure2.png', align = "center"),
       img(src='/images/figure3.png', align = "center"),
       img(src='/images/figure4.png', align = "center"),
-      img(src='/images/figure5.png', align = "center")
+      img(src='/images/figure5.png', align = "center"),
+      
+      hr(),
+      
+      p('Research efforts are currently ongoing using additional mouse models of autism, adding to the long list of strains which were scanned 
+        and analyzed in the original work.  This research could not have been done without the help from a long list of people, 
+        who include:'),
+      
+      tags$ul(
+        tags$li('E Anagnostou'), 
+        tags$li('BA Babineau'), 
+        tags$li('JN Crawley'),
+        tags$li('L Lin'),
+        tags$li('M Genestine'),
+        tags$li('E DiCicco-Bloom'),
+        tags$li('JKY Lai'),
+        tags$li('JA Foster'),
+        tags$li('O Peñagarikano'),
+        tags$li('DH Geschwind'),
+        tags$li('LK Pacey'),
+        tags$li('DR Hampson'),
+        tags$li('CL Laliberté'),
+        tags$li('AA Mills'),
+        tags$li('E Tam'),
+        tags$li('LR Osborne'),
+        tags$li('M Kouser'),
+        tags$li('F Espinosa-Becerra'),
+        tags$li('Z Xuan'),
+        tags$li('CM Powell'),
+        tags$li('A Raznahan'),
+        tags$li('DM Robins'),
+        tags$li('N Nakai'),
+        tags$li('J Nakatani'),
+        tags$li('T Takumi'),
+        tags$li('MC van Eede'),
+        tags$li('TM Kerr'),
+        tags$li('C Muller'),
+        tags$li('RD Blakely'),
+        tags$li('J Veenstra-VanderWeele'),
+        tags$li('RM Henkelman'),
+        tags$li('JP Lerch')  
+      ),
+      
+      p('In addition to the long list of people who contributed to this work, several institutions were involved in the study\'s funding:'),
+      
+      tags$ul(
+        tags$li('Canadian Institute for Health Research (CIHR)'), 
+        tags$li('Ontario Brain Institute (OBI)'), 
+        tags$li('Ontario Mental Health Foundation (OHMF')
+      )
       
     ),
     
@@ -48,9 +91,9 @@ shinyUI(
       p('Select a subset of mouse strains and brain regions that you are interested in investigating further, and then click Recalculate 
         to see which mouse strains and brain regions cluster together.'),
       
-      p('Note that the heatmap displayed here will be slightly different than Figure 3 found in the original paper, because Figure 3 uses
-        the median bootstrapped effect sizes whereas this figure uses the original data without any bootstrapping.  Unfortunately,
-        the bootstrapping procedure is too computationally intensive to present in this web app.'),
+      p('Note that the heatmap displayed here will be slightly different than Figure 3 depicted in the original paper, because Figure 3 uses
+        the median bootstrapped effect sizes whereas this figure uses the original data without any statistical bootstrapping.  Unfortunately,
+        the bootstrapping procedure is too computationally intensive to present in the context of this web app.'),
 
       hr(),
       
